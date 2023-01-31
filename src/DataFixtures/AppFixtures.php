925e8663be59e7dbb411e7b0909b4100436ea76a
Elliot Curvat <91797716+ECurvat@users.noreply.github.com>
2023-01-31 23:11:10 +0100

    ✨ Entité Contrat + Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Contrat;
 use App\Entity\Service;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -16,14 +17,21 @@ class AppFixtures extends Fixture
             ->setDepot('MEY')
             ->setDebut(new \DateTime('2023-01-21 16:16:00'))
             ->setFin(new \DateTime('2023-01-21 23:36:00'))
-            ->setPause(new \DateTime('2023-01-21 00:25:00'))
-            ->setDispo(new \DateTime('2023-01-21 00:00:00'))
-            ->setDeplacement(new \DateTime('2023-01-21 00:00:00'))
+            ->setPause(new \DateTime('2023-01-21 00:43:00'))
+            ->setDispo(new \DateTime('2023-01-21 00:07:00'))
+            ->setDeplacement(new \DateTime('2023-01-21 00:25:00'))
             ->setCoupure(new \DateTime('2023-01-21 00:00:00'))
             ->setLigne(4);
 
             $manager->persist($service);
         }
+
+        $contrat = new Contrat();
+        $contrat->setDebut(new \DateTime('2023-01-12 00:00:00'))
+        ->setFin(new \DateTime('2023-01-12 00:00:00'))
+        ->setId(20233);
+
+        $manager->persist($contrat);
 
         $manager->flush();
     }
---
+++ src/Entity/Contrat.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContratRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContratRepository::class)]
+class Contrat
+{
+    #[ORM\Id]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $debut = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $fin = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId($id): ?self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getDebut(): ?\DateTimeInterface
+    {
+        return $this->debut;
+    }
+
+    public function setDebut(\DateTimeInterface $debut): self
+    {
+        $this->debut = $debut;
+
+        return $this;
+    }
+
+    public function getFin(): ?\DateTimeInterface
+    {
+        return $this->fin;
+    }
+
+    public function setFin(\DateTimeInterface $fin): self
+    {
+        $this->fin = $fin;
+
+        return $this;
+    }
+
+}
